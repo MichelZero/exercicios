e811203ae81bbef5de3d165ef084c1aeffc04078
Michel <106674127+MichelZero@users.noreply.github.com>
2022-09-09 19:31:47 -0300

    up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {Text} from 'react-native';
 
 // o nome do componente vamos iniciar em maiculos e pode ser qq nome.
 // o nome App não tem nada haver com o nome do arquivo, podemos usar
-// Teste, desde que exporte Teste
+// Teste, desde que exporte Teste.
+
 /* function Teste() {
   return <Text>Primeiro Componente</Text>;
 }
